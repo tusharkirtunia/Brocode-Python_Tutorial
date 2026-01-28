be8dd625559a729c5d97a4ab1647b7f0a449a416
Tushar Kirtunia <tushar.kirtunia.work@gmail.com>
2026-01-28 17:14:53 +0530

    new commits

--- Basics/3_userinput.py
+++ Basics/3_userinput.py
@@ -1,8 +1,8 @@
 # input() = a function that prompts the user to enter data
 #                returns the enter data as a string
 
-name = input("What is your name?")
-age = int(input("How old are you?"))
+name = input("What is your name? ")
+age = int(input("How old are you? "))
 
 age = age + 1
 
